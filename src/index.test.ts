fd9d81fd0647640419ef36593e0b07e3a220f7d9
sosuisen <hidekazu.kubota@gmail.com>
2021-01-07 20:16:34 +0900

    Add GitDocumentDB class and the first test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,35 @@
 
-import { sum } from './index';
+import { GitDocumentDB } from './index';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1,2)).toBe(3);
+let gitDDB: GitDocumentDB = new GitDocumentDB('ddb_test_repository01');
+
+test('Create a new document', () => {
+  expect(gitDDB.put({ id: 'prof01', name: 'shirase' })).toEqual({ id: 'prof01', name: 'shirase'});
 });
+
+test('Update an existing document', () => {
+  expect(gitDDB.put({ id: 'prof01', name: 'mari' })).toEqual({ id: 'prof01', name: 'mari' });
+});
+
+test('Fetch a document', () => {
+  expect(gitDDB.get('prof01')).toEqual({ id: 'prof01', name: 'mari'});
+})
+
+test('Delete the document', () => {
+  expect(gitDDB.delete('prof01')).toEqual({ id: 'prof01', name: 'mari'});
+})
+
+test('Create another document', () => {
+  expect(gitDDB.put({ id: 'prof02', name: 'yuzu' })).toEqual({ id: 'prof02', name: 'yuzu'});
+});
+
+test('Close the database', () => {
+  expect(gitDDB.close()).toBeTruthy();
+  expect(gitDDB.put({ id: 'prof01', name: 'shirase' })).toThrowError();
+})
+
+gitDDB = new GitDocumentDB('ddb_test_repository01');
+
+test('Open existing database and fetch a document', () => {
+  expect(gitDDB.get('prof02')).toEqual({ id: 'prof02', name: 'yuzu'});
+})
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
-export function sum (a: number, b: number) {
-  return a + b;
-}
 
-console.log(sum (3, 4));
+export class GitDocumentDB {
+
+  /**
+   * Create a database of opens an existing one.
+   */
+  constructor(databaseName: string) {
+
+  }
+  
+  put = (doc: object) => {
+    return doc;
+  };
+
+  get = (id: string) => {
+    const doc = { id: 'prof01', name: 'mari'};
+    return doc;
+  };
+
+  delete = (id: string) => {
+    const doc = { id: 'prof01', name: 'mari' };
+    return doc;
+  };
+
+  close = () => {
+    return true;
+  }
+}
