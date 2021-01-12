9869942cbe08852d69afa3222fa30f5d5bc8561f
sosuisen <hidekazu.kubota@gmail.com>
2021-01-12 18:06:31 +0900

    [add] Add test for creating non-existent directory

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,44 +1,44 @@
 
 import fs from 'fs-extra';
 import path from 'path';
-import { GitDocumentDB, example } from './index';
+import { CannotCreateDirectoryError } from './error';
+import { GitDocumentDB } from './index';
 
-describe('Create database', () => {
+describe('Create database (1)', () => {
+  const readonlyDir = './test_repos01_readonly/';
   const localDir = './test_repos01';
   const dbName = 'ddb_test_repository01';
-
-  const gitDDB: GitDocumentDB = new GitDocumentDB({
-    dbName: dbName,
-    localDir: localDir
-  });
-    
+   
   beforeAll(() => {
-    fs.removeSync(path.resolve(localDir));
+    if(process.platform !== 'win32') {
+      fs.removeSync(path.resolve(readonlyDir));
+    }
   });
 
   afterAll(() => {
-    fs.removeSync(path.resolve(localDir));
+    if(process.platform !== 'win32') {
+      fs.removeSync(path.resolve(readonlyDir));
+    }
   });
 
-  test('Create and destroy a new database', async () => {
-    // Path not found
-    await expect(gitDDB.open()).rejects.toMatchObject({ name: 'Error' });
-
-    // Create directory
-    fs.mkdirSync(path.resolve(localDir));
-
-    // Create db
-    await expect(gitDDB.open()).resolves.toMatchObject({ isNew: true });
-
-    // Destroy db
-    await expect(gitDDB.destroy()).resolves.toBeTruthy();
-    // fs.access() throw error when a file cannot be accessed.    
-    await expect(fs.access(path.resolve(localDir, dbName))).rejects.toMatchObject({ name: 'Error', code: 'ENOENT' });
+  test('Create a new database', async () => {
+      // Windows does not support permission option of fs.mkdir().
+    if(process.platform === 'win32') {
+      console.log(`You must create ${readonlyDir} directory by hand and remove write permission of Authenticated Users.`);
+    }
+    else{
+      await fs.mkdir(readonlyDir, {mode: 0o400});
+    }
+    const gitDDB: GitDocumentDB = new GitDocumentDB({
+      dbName: dbName,
+      localDir: readonlyDir + localDir
+    });
+    // You don't have permission
+    await expect(gitDDB.open()).rejects.toBeInstanceOf(CannotCreateDirectoryError);
   });
 });
 
-
-describe('Open and close database', () => {
+describe('Create database (2)', () => {
   const localDir = './test_repos02';
   const dbName = 'ddb_test_repository02';
 
@@ -55,9 +55,36 @@ describe('Open and close database', () => {
     fs.removeSync(path.resolve(localDir));
   });
 
+  test('Create a new database', async () => {
+    // Create db
+    await expect(gitDDB.open()).resolves.toMatchObject({ isNew: true });
+    // Destroy db
+    await expect(gitDDB.destroy()).resolves.toBeTruthy();
+    // fs.access() throw error when a file cannot be accessed.    
+    await expect(fs.access(path.resolve(localDir, dbName))).rejects.toMatchObject({ name: 'Error', code: 'ENOENT' });
+  });
+});
+
+
+describe('Open and close database', () => {
+  const localDir = './test_repos03';
+  const dbName = 'ddb_test_repository03';
+
+  const gitDDB: GitDocumentDB = new GitDocumentDB({
+    dbName: dbName,
+    localDir: localDir
+  });
+
+  beforeAll(() => {
+    fs.removeSync(path.resolve(localDir));
+  });
+
+  afterAll(() => {
+    fs.removeSync(path.resolve(localDir));
+  });
+
   test('Open and close the existing database', async () => {
     // Create db
-    fs.mkdirSync(path.resolve(localDir));
     await gitDDB.open();
 
     // Close created db
@@ -74,8 +101,8 @@ describe('Open and close database', () => {
 });
 
 describe('CRUD', () => {
-  const localDir = './test_repos03';
-  const dbName = 'ddb_test_repository03';
+  const localDir = './test_repos04';
+  const dbName = 'ddb_test_repository04';
 
   const gitDDB: GitDocumentDB = new GitDocumentDB({
     dbName: dbName,
@@ -92,7 +119,6 @@ describe('CRUD', () => {
 
   test('Create a new document', async () => {
     // Create db
-    fs.mkdirSync(path.resolve(localDir));
     await gitDDB.open();
 
     await expect(gitDDB.put({ id: 'prof01', name: 'shirase' })).resolves.toMatch(/^[a-z0-9]{40}$/);
