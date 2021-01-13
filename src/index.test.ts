e5b8bc138ed1b769fb0c900b5eb9420912d42ca5
sosuisen <hidekazu.kubota@gmail.com>
2021-01-13 22:09:25 +0900

    [add] Add test for isCreatedByGitDB and isValidVersion

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,32 +3,55 @@ import fs from 'fs-extra';
 import path from 'path';
 import { CannotCreateDirectoryError } from './error';
 import { GitDocumentDB } from './index';
+import nodegit from 'nodegit';
 
-const localDir = './test/database';
+
+interface RepositoryInitOptions {
+  description?: string;
+  initialHead?: string;
+  flags?: number; // https://libgit2.org/libgit2/#HEAD/type/git_repository_init_flag_t
+  mode?: number; // https://libgit2.org/libgit2/#HEAD/type/git_repository_init_mode_t
+  originUrl?: string;
+  templatePath?: string;
+  version?: number;
+  workdirPath?: string;
+}
+
+const repositoryInitOptionFlags = {
+  GIT_REPOSITORY_INIT_BARE: 1,
+  GIT_REPOSITORY_INIT_NO_REINIT: 2,
+  GIT_REPOSITORY_INIT_NO_DOTGIT_DIR: 4,
+  GIT_REPOSITORY_INIT_MKDIR: 8,
+  GIT_REPOSITORY_INIT_MKPATH: 16,
+  GIT_REPOSITORY_INIT_EXTERNAL_TEMPLATE: 32,
+  GIT_REPOSITORY_INIT_RELATIVE_GITLINK: 64,
+};
+
 
 describe('Create database (1)', () => {
   const readonlyDir = './test/readonly/';
+  const localDir = './test/database01';
   const dbName = './test_repos01';
-   
+
   beforeAll(() => {
-    if(process.platform !== 'win32') {
+    if (process.platform !== 'win32') {
       fs.removeSync(path.resolve(readonlyDir));
     }
   });
 
   afterAll(() => {
-    if(process.platform !== 'win32') {
+    if (process.platform !== 'win32') {
       fs.removeSync(path.resolve(readonlyDir));
     }
   });
 
   test('Create a new database', async () => {
-      // Windows does not support permission option of fs.mkdir().
-    if(process.platform === 'win32') {
-      console.log(`You must create ${readonlyDir} directory by hand, click [disable inheritance] button, and remove write permission of Authenticated Users.`);
+    // Windows does not support permission option of fs.mkdir().
+    if (process.platform === 'win32') {
+      console.warn(`You must create ${readonlyDir} directory by hand, click [disable inheritance] button, and remove write permission of Authenticated Users.`);
     }
-    else{
-      await fs.mkdir(readonlyDir, {mode: 0o400});
+    else {
+      await fs.mkdir(readonlyDir, { mode: 0o400 });
     }
     const gitDDB: GitDocumentDB = new GitDocumentDB({
       dbName: dbName,
@@ -40,6 +63,7 @@ describe('Create database (1)', () => {
 });
 
 describe('Create database (2)', () => {
+  const localDir = './test/database02';
   const dbName = './test_repos02';
 
   const gitDDB: GitDocumentDB = new GitDocumentDB({
@@ -67,6 +91,7 @@ describe('Create database (2)', () => {
 
 
 describe('Open and close database', () => {
+  const localDir = './test/database03';
   const dbName = './test_repos03';
 
   const gitDDB: GitDocumentDB = new GitDocumentDB({
@@ -82,7 +107,7 @@ describe('Open and close database', () => {
     fs.removeSync(path.resolve(localDir));
   });
 
-  test('Open and close the existing database', async () => {
+  test('Open and close an existing database', async () => {
     // Create db
     await gitDDB.open();
 
@@ -97,9 +122,55 @@ describe('Open and close database', () => {
     await expect(gitDDB.destroy()).resolves.toBeTruthy();
     expect(gitDDB.isOpened()).toBeFalsy();
   });
+
+
+  test('Open a database created by another app', async () => {
+    const dbNameA = 'test_repos03A';
+    const gitDDB_A: GitDocumentDB = new GitDocumentDB({
+      dbName: dbNameA,
+      localDir: localDir
+    });
+    const optionsA: RepositoryInitOptions = {
+      description: 'another app',
+      flags: repositoryInitOptionFlags.GIT_REPOSITORY_INIT_MKDIR,
+      initialHead: 'main'
+    };
+    // Create git repository with invalid description
+    await fs.ensureDir(localDir).catch((err: Error) => { throw new CannotCreateDirectoryError(err.message); });
+    // @ts-ignore
+    await nodegit.Repository.initExt(path.resolve(localDir, dbNameA), optionsA).catch(err => { throw new Error(err) });
+    gitDDB.close();
+
+    await expect(gitDDB_A.open()).resolves.toMatchObject({ isNew: false, isCreatedByGitDDB: false, isValidVersion: false });
+    await gitDDB_A.destroy();
+  });
+
+
+  test('Open a database created by another version', async () => {
+    const dbNameB = 'test_repos03B';
+    const gitDDB_B: GitDocumentDB = new GitDocumentDB({
+      dbName: dbNameB,
+      localDir: localDir
+    });
+    const optionsB: RepositoryInitOptions = {
+      description: 'GitDocumentDB: 0.1',
+      flags: repositoryInitOptionFlags.GIT_REPOSITORY_INIT_MKDIR,
+      initialHead: 'main'
+    };
+    // Create git repository with invalid description
+    await fs.ensureDir(localDir).catch((err: Error) => { throw new CannotCreateDirectoryError(err.message); });
+    // @ts-ignore
+    await nodegit.Repository.initExt(path.resolve(localDir, dbNameB), optionsB).catch(err => { throw new Error(err) });
+    gitDDB.close();
+
+    await expect(gitDDB_B.open()).resolves.toMatchObject({ isNew: false, isCreatedByGitDDB: true, isValidVersion: false });
+    await gitDDB_B.destroy();
+  });  
 });
 
+
 describe('CRUD', () => {
+  const localDir = './test/database04';
   const dbName = './test_repos04';
 
   const gitDDB: GitDocumentDB = new GitDocumentDB({
@@ -128,24 +199,24 @@ describe('CRUD', () => {
      */
   });
 
-/*
-  test('Update an existing document', () => {
-    expect(gitDDB.put({ id: 'prof01', name: 'mari' })).toEqual({ id: 'prof01', name: 'mari' });
-  });
-
-
-  test('Fetch a document', () => {
-    expect(gitDDB.get('prof01')).toEqual({ id: 'prof01', name: 'mari' });
-  })
-
-
-  test('Delete the document', () => {
-    expect(gitDDB.delete('prof01')).toEqual({ id: 'prof01', name: 'mari' });
-  })
-
-
-  test('Create another document', () => {
-    expect(gitDDB.put({ id: 'prof02', name: 'yuzu' })).toEqual({ id: 'prof02', name: 'yuzu' });
-  });
-*/
+  /*
+    test('Update an existing document', () => {
+      expect(gitDDB.put({ id: 'prof01', name: 'mari' })).toEqual({ id: 'prof01', name: 'mari' });
+    });
+  
+  
+    test('Fetch a document', () => {
+      expect(gitDDB.get('prof01')).toEqual({ id: 'prof01', name: 'mari' });
+    })
+  
+  
+    test('Delete the document', () => {
+      expect(gitDDB.delete('prof01')).toEqual({ id: 'prof01', name: 'mari' });
+    })
+  
+  
+    test('Create another document', () => {
+      expect(gitDDB.put({ id: 'prof02', name: 'yuzu' })).toEqual({ id: 'prof02', name: 'yuzu' });
+    });
+  */
 });
