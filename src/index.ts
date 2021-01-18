2d74bc5f897d0d8fc47d6aed8325d4941670c87b
sosuisen <hidekazu.kubota@gmail.com>
2021-01-19 00:38:40 +0900

    [add] Catch DocumentNotFoundError from commit.getEntry()

--- src/index.ts
+++ src/index.ts
@@ -266,7 +266,7 @@ export class GitDocumentDB {
     }
     else {
       const commit = await this._currentRepository.getCommit(head as nodegit.Oid); // get the commit of HEAD
-      const entry = await commit.getEntry(_id);
+      const entry = await commit.getEntry(_id).catch(err => { throw new DocumentNotFoundError(err.message)});
       if (entry) {
         const blob = await entry.getBlob();
         try {
