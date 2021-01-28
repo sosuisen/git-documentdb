57fd2cc59c174777203b4ddb33f0ada2e3f543ce
sosuisen <hidekazu.kubota@gmail.com>
2021-01-28 22:03:17 +0900

    [fix] Update examples

--- examples/src/index.ts
+++ examples/src/index.ts
@@ -1,24 +1,23 @@
-import { GitDocumentDB, CannotCreateDirectoryError } from 'git-documentdb';
-
-/**
- * Test readonly directory
- */ 
-// const localDir = '../test_repos01_readonly/test_repos_example';
-/**
- * Use writable directory
- */ 
-const localDir = './test_repos_example';
-const dbName = 'ddb_test_repository';
+import { GitDocumentDB } from 'git-documentdb';
 
 const gitDDB = new GitDocumentDB({
-  dbName: dbName,
-  localDir: localDir
-});
-
-gitDDB.open().then(() => {
-  gitDDB.put({ id: 'prof01', name: 'shirase' })
-}).catch(err => {
-  if (err instanceof CannotCreateDirectoryError) {
-    console.log(`Error: ${err.message}`);
-  }
-});
+    localDir: 'gddb_data',
+    dbName: 'db01',
+  });
+const setAndGetProf = async () => {
+  // Create repository
+  await gitDDB.open();
+  // Create
+  await gitDDB.put({ _id: '4', name: 'Yuzuki', age: '15' });
+  // Update
+  await gitDDB.put({ _id: '4', name: 'Yuzuki', age: '16' });
+  // Read
+  const prof = await gitDDB.get('4');
+  console.log(prof);  // { _id: '4', name: 'Yuzuki', age: '16' }
+  // Delete
+  await gitDDB.delete('4');
+  await gitDDB.close();
+  // destroy() removes repository
+  // await gitDDB.destroy(); 
+}
+setAndGetProf();
