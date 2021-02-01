ee345d1c8bb7a62ba8802fcf3de0664913775999
sosuisen <hidekazu.kubota@gmail.com>
2021-02-01 17:51:48 +0900

    [change] Change currentRepository() to getRepository()

--- test/allDocs.test.ts
+++ test/allDocs.test.ts
@@ -346,7 +346,7 @@ describe('Fetch a batch of documents', () => {
     const _id = 'invalidJSON';
     let file_sha, commit_sha: string;
     const data = 'invalid data'; // JSON.parse() will throw error
-    const _currentRepository = gitDDB.currentRepository();
+    const _currentRepository = gitDDB.getRepository();
     if (_currentRepository) {
       try {
         const filePath = path.resolve(gitDDB.workingDir(), _id);
