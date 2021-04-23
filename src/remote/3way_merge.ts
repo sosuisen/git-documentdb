b864c7f3790c6b9f37c82f720996cb03020ca44d
sosuisen <hidekazu.kubota@gmail.com>
2021-04-23 18:07:00 +0900

    fix: extract threeWayMerge from sync_worker.ts to 3way_merge.ts

---
+++ src/remote/3way_merge.ts
@@ -0,0 +1,314 @@
+import nodePath from 'path';
+import nodegit from '@sosuisen/nodegit';
+import fs from 'fs-extra';
+import {
+  CannotCreateDirectoryError,
+  CannotDeleteDataError,
+  InvalidConflictStateError,
+  RepositoryNotOpenError,
+} from '../error';
+import { AcceptedConflict, ConflictResolveStrategies } from '../types';
+import { AbstractDocumentDB } from '../types_gitddb';
+import { getDocument } from './worker_utils';
+
+/**
+ * Write blob to file system
+ *
+ * @throws {@link CannotCreateDirectoryError}
+ */
+async function writeBlobToFile (gitDDB: AbstractDocumentDB, entry: nodegit.TreeEntry) {
+  const data = (await entry.getBlob()).toString();
+  const filename = entry.name();
+  const filePath = nodePath.resolve(gitDDB.workingDir(), filename);
+  const dir = nodePath.dirname(filePath);
+  await fs.ensureDir(dir).catch((err: Error) => {
+    return Promise.reject(new CannotCreateDirectoryError(err.message));
+  });
+  await fs.writeFile(filePath, data);
+}
+
+async function getStrategy (
+  gitDDB: AbstractDocumentDB,
+  strategy: ConflictResolveStrategies | undefined,
+  path: string,
+  ours?: nodegit.TreeEntry,
+  theirs?: nodegit.TreeEntry
+) {
+  const defaultStrategy: ConflictResolveStrategies = 'ours-prop';
+  if (strategy === undefined) {
+    strategy = defaultStrategy;
+  }
+  else if (
+    strategy !== 'ours-prop' &&
+    strategy !== 'theirs-prop' &&
+    strategy !== 'ours' &&
+    strategy !== 'theirs'
+  ) {
+    // Strategy may be a function
+    const id = path.replace(new RegExp(gitDDB.fileExt + '$'), '');
+    const oursDoc = ours
+      ? await getDocument(gitDDB, id, ours.id()).catch(() => undefined)
+      : undefined;
+    const theirsDoc = theirs
+      ? await getDocument(gitDDB, id, theirs.id()).catch(() => undefined)
+      : undefined;
+    strategy = strategy(oursDoc, theirsDoc);
+    if (strategy === undefined) {
+      strategy = defaultStrategy;
+    }
+  }
+  return strategy;
+}
+
+/**
+ * 3-way merge
+ *
+ * @throws {@link RepositoryNotOpenError}
+ * @throws {@link InvalidConflictStateError}
+ * @throws {@link CannotDeleteDataError}
+ * @throws {@link CannotCreateDirectoryError} (from writeBlobToFile())
+ */
+// eslint-disable-next-line complexity
+export async function threeWayMerge (
+  gitDDB: AbstractDocumentDB,
+  conflict_resolve_strategy: ConflictResolveStrategies,
+  resolvedIndex: nodegit.Index,
+  path: string,
+  mergeBase: nodegit.Commit,
+  oursCommit: nodegit.Commit,
+  theirsCommit: nodegit.Commit,
+  acceptedConflicts: AcceptedConflict[]
+): Promise<void> {
+  const repos = gitDDB.repository();
+  if (repos === undefined) {
+    throw new RepositoryNotOpenError();
+  }
+  // Try 3-way merge on the assumption that their is no conflict.
+  const baseCommit = await repos.getCommit(mergeBase);
+
+  const ours = await oursCommit.getEntry(path).catch(() => undefined);
+  const theirs = await theirsCommit.getEntry(path).catch(() => undefined);
+  const base = await baseCommit.getEntry(path).catch(() => undefined);
+
+  const docId = path.replace(new RegExp(gitDDB.fileExt + '$'), '');
+
+  // 2 x 2 x 2 cases
+  if (!base && !ours && !theirs) {
+    // This case must not occurred.
+    throw new InvalidConflictStateError(
+      'Neither a base entry nor a local entry nor a remote entry exists.'
+    );
+  }
+  else if (!base && !ours && theirs) {
+    // A new file has been created on theirs.
+    // Write it to the file.
+    // console.log(' #case 1 - Accept theirs (create): ' + path);
+    await writeBlobToFile(gitDDB, theirs);
+    await resolvedIndex.addByPath(path);
+  }
+  else if (!base && ours && !theirs) {
+    // A new file has been created on ours.
+    // Just add it to the index.
+    // console.log(' #case 2 - Accept ours (create): ' + path);
+    await resolvedIndex.addByPath(path);
+  }
+  else if (!base && ours && theirs) {
+    if (ours.id().equal(theirs.id())) {
+      // The same filenames with exactly the same contents are created on both local and remote.
+      // console.log(' #case 3 - Accept both (create): ' + path);
+      // Jut add it to the index.
+      await resolvedIndex.addByPath(path);
+    }
+    else {
+      // ! Conflict
+      const strategy = await getStrategy(
+        gitDDB,
+        conflict_resolve_strategy,
+        path,
+        ours,
+        theirs
+      );
+      if (strategy === 'ours') {
+        // Just add it to the index.
+        // console.log(' #case 4 - Conflict. Accept ours (create): ' + path);
+        acceptedConflicts.push({
+          target: {
+            id: docId,
+            file_sha: ours.sha(),
+          },
+          strategy: 'ours',
+          operation: 'create',
+        });
+        await resolvedIndex.addByPath(path);
+      }
+      else if (strategy === 'theirs') {
+        // Write theirs to the file.
+        // console.log(' #case 5 - Conflict. Accept theirs (create): ' + path);
+        acceptedConflicts.push({
+          target: {
+            id: docId,
+            file_sha: theirs.sha(),
+          },
+          strategy: 'theirs',
+          operation: 'create',
+        });
+        await writeBlobToFile(gitDDB, theirs);
+        await resolvedIndex.addByPath(path);
+      }
+    }
+  }
+  else if (base && !ours && !theirs) {
+    // The same files are removed.
+    // console.log(' #case 6 - Accept both (delete): ' + path);
+    await resolvedIndex.removeByPath(path);
+  }
+  else if (base && !ours && theirs) {
+    if (base.id().equal(theirs.id())) {
+      // A file has been removed on ours.
+      // console.log(' #case 7 - Accept ours (delete): ' + path);
+      await resolvedIndex.removeByPath(path);
+    }
+    else {
+      // ! Conflict
+      const strategy = await getStrategy(
+        gitDDB,
+        conflict_resolve_strategy,
+        path,
+        ours,
+        theirs
+      );
+      if (strategy === 'ours') {
+        // Just add it to the index.
+        // console.log(' #case 8 - Conflict. Accept ours (delete): ' + path);
+        acceptedConflicts.push({
+          target: {
+            id: docId,
+            file_sha: base.sha(),
+          },
+          strategy: 'ours',
+          operation: 'delete',
+        });
+        await resolvedIndex.removeByPath(path);
+      }
+      else if (strategy === 'theirs') {
+        // Write theirs to the file.
+        // console.log(' #case 9 - Conflict. Accept theirs (update): ' + path);
+        acceptedConflicts.push({
+          target: {
+            id: docId,
+            file_sha: theirs.sha(),
+          },
+          strategy: 'theirs',
+          operation: 'update',
+        });
+        await writeBlobToFile(gitDDB, theirs);
+        await resolvedIndex.addByPath(path);
+      }
+    }
+  }
+  else if (base && ours && !theirs) {
+    if (base.id().equal(ours.id())) {
+      // A file has been removed on theirs.
+      // console.log(' #case 10 - Accept theirs (delete): ' + path);
+      await fs.remove(nodePath.resolve(repos.workdir(), path)).catch(() => {
+        throw new CannotDeleteDataError();
+      });
+      await resolvedIndex.removeByPath(path);
+    }
+    else {
+      // ! Conflict
+      const strategy = await getStrategy(
+        gitDDB,
+        conflict_resolve_strategy,
+        path,
+        ours,
+        theirs
+      );
+      if (strategy === 'ours') {
+        // Just add to the index.
+        // console.log(' #case 11 - Conflict. Accept ours (update): ' + path);
+        acceptedConflicts.push({
+          target: {
+            id: docId,
+            file_sha: ours.sha(),
+          },
+          strategy: 'ours',
+          operation: 'update',
+        });
+        await resolvedIndex.addByPath(path);
+      }
+      else if (strategy === 'theirs') {
+        // Remove file
+        // console.log(' #case 12 - Conflict. Accept theirs (delete): ' + path);
+        acceptedConflicts.push({
+          target: {
+            id: docId,
+            file_sha: base.sha(),
+          },
+          strategy: 'theirs',
+          operation: 'delete',
+        });
+        await fs.remove(nodePath.resolve(repos.workdir(), path)).catch(() => {
+          throw new CannotDeleteDataError();
+        });
+        await resolvedIndex.removeByPath(path);
+      }
+    }
+  }
+  else if (base && ours && theirs) {
+    if (ours.id().equal(theirs.id())) {
+      // The same filenames with exactly the same contents are created on both local and remote.
+      // Jut add it to the index.
+      // console.log(' #case 13 - Accept both (update): ' + path);
+      await resolvedIndex.addByPath(path);
+    }
+    else if (base.id().equal(ours.id())) {
+      // Write theirs to the file.
+      // console.log(' #case 14 - Accept theirs (update): ' + path);
+      await writeBlobToFile(gitDDB, theirs);
+      await resolvedIndex.addByPath(path);
+    }
+    else if (base.id().equal(theirs.id())) {
+      // Jut add it to the index.
+      // console.log(' #case 15 - Accept ours (update): ' + path);
+      await resolvedIndex.addByPath(path);
+    }
+    else {
+      // ! Conflict
+      const strategy = await getStrategy(
+        gitDDB,
+        conflict_resolve_strategy,
+        path,
+        ours,
+        theirs
+      );
+      if (strategy === 'ours') {
+        // Just add it to the index.
+        // console.log(' #case 16 - Conflict. Accept ours (update): ' + path);
+        acceptedConflicts.push({
+          target: {
+            id: docId,
+            file_sha: ours.sha(),
+          },
+          strategy: 'ours',
+          operation: 'update',
+        });
+        await resolvedIndex.addByPath(path);
+      }
+      else if (strategy === 'theirs') {
+        // Write theirs to the file.
+        // console.log(' #case 17 - Conflict. Accept theirs (update): ' + path);
+        acceptedConflicts.push({
+          target: {
+            id: docId,
+            file_sha: theirs.sha(),
+          },
+          strategy: 'theirs',
+          operation: 'update',
+        });
+        await writeBlobToFile(gitDDB, theirs);
+        await resolvedIndex.addByPath(path);
+      }
+    }
+  }
+}
