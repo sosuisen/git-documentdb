c07ba1e6ef2864e99525f5f0c07a5d0372ac3b60
sosuisen <hidekazu.kubota@gmail.com>
2021-04-23 16:44:30 +0900

    fix: extract examples about collection from example/src/index.ts
    Add examples for revisions.
    Update console.log().

--- examples/src/index.ts
+++ examples/src/index.ts
@@ -18,17 +18,39 @@ const gitddb_example = async () => {
   if (!result.ok) await gitDDB.create(); // Git creates and opens a repository if not exits.
   // Create
   await gitDDB.put({ _id: 'nara', flower: 'cherry blossoms', season: 'spring' }); // Git adds 'nara.json' under the working directory and commits it.
+
+  console.log(`$ gitDDB.put({ _id: 'nara' ... }) # Create`);
+  console.log(await gitDDB.get('nara')); // { _id: 'nara', flower: 'cherry blossoms', season: 'spring' }
+  
+
   // Update
   await gitDDB.put({ _id: 'nara', flower: 'double cherry blossoms', season: 'spring' }); // Git adds an updated file and commits it.
+
+
   // Read
   const doc = await gitDDB.get('nara');
-  console.log(doc); // doc = { flower: 'double cherry blossoms', season: 'spring', _id: 'nara' }
-  // Delete
-  await gitDDB.remove('nara'); // Git removes a file and commits it.
 
+  console.log(`\n$ gitDDB.put({ _id: 'nara' ... }) # Update`);
+  console.log(doc); // { flower: 'double cherry blossoms', season: 'spring', _id: 'nara' }
+
+
+  // Delete
+  await gitDDB.delete('nara'); // Git deletes a file and commits it. You can call remove() as alias for delete().
+
+  console.log(`\n$ gitDDB.delete('nara') # Delete`);
+  console.log(await gitDDB.get('nara')); // undefined
+  
+
+  // Revisions 
+  // get(id, 2) returns two revisions before
+  const oldDoc = await gitDDB.get('nara', 2); 
+
+  console.log(`\n$ gitDDB.get('nara', 2) # Get two revisions before`);
+  console.log(oldDoc); // { _id: 'nara', flower: 'cherry blossoms', season: 'spring' }
+  
   /* Where is the working directory?
   const workingDir = gitDDB.workingDir();
-  console.log(workingDir); // workingDir = '/your/path/to/the/example/git-documentdb/db01'
+  console.log(workingDir); // '/your/path/to/the/example/git-documentdb/db01'
   */
 
   /**
@@ -43,6 +65,7 @@ const gitddb_example = async () => {
       remote_url: github_repository,
       connection: { type: 'github', personal_access_token: your_github_personal_access_token },
     });
+    console.log('\n# Start sync..');
     // git-documentdb-example.git is automatically created in your GitHub account.
     // The data will be synchronized every 10 seconds.
     // Check below if you fail:
@@ -66,16 +89,26 @@ const gitddb_example = async () => {
   // Put documents by using filepath.
   await gitDDB.put({ _id: 'nara/nara_park', flower: 'double cherry blossoms' });
   await gitDDB.put({ _id: 'nara/tsukigase', flower: 'Japanese apricot' });
-  await gitDDB.put({ _id: 'yoshino/mt_yoshino', flower: 'cherry blossoms' });
+  await gitDDB.put({ _id: 'yoshino/mt_yoshino', flower: 'awesome cherry blossoms' });
+
+  console.log(`\n$ gitDDB.put({ _id: 'nara/nara_park' ... }) # Put into subdirectory`);
+  console.log(`$ gitDDB.put({ _id: 'nara/tsukigase' ... })`);
+  console.log(`$ gitDDB.put({ _id: 'yoshino/mt_yoshino' ... })`);
+
 
   // Read
   const flowerInYoshino = await gitDDB.get('yoshino/mt_yoshino');
-  console.log(flowerInYoshino); // flowerInYoshino = { flower: 'cherry blossoms', _id: 'yoshino/mt_yoshino' }
+
+  console.log(`$ gitDDB.get('yoshino/mt_yoshino') # Get from subdirectory`);
+  console.log(flowerInYoshino); // { flower: 'awesome cherry blossoms', _id: 'yoshino/mt_yoshino' }
+
 
   // Prefix search
   
   // Read all the documents whose IDs start with the prefix.
   const flowersInNara = await gitDDB.allDocs({ prefix: 'nara/', include_docs: true });
+
+  console.log(`\n$ gitDDB.allDocs({ prefix: 'nara/', include_docs: true }) # Prefix search`);
   console.dir(flowersInNara, { depth: 3 });
   /* flowersInNara = 
   {
@@ -95,66 +128,8 @@ const gitddb_example = async () => {
     ]
   }
   */
+
   // Close database
-  await gitDDB.close();
-
-
-  // Try them again by another way. 
-  gitDDB = new GitDocumentDB({
-    db_name: 'db02',
-  });
-  await gitDDB.destroy(); // destroy() closes db and removes both the Git repository and the working directory if exist.
-
-  await gitDDB.create();
-  // Use collections to make it easier
-  const nara = gitDDB.collection('nara');
-  const yoshino = gitDDB.collection('yoshino');
-  await nara.put({ _id: 'nara_park', flower: 'double cherry blossoms' });
-  await nara.put({ _id: 'tsukigase', flower: 'Japanese apricot' });
-  await yoshino.put({ _id: 'mt_yoshino', flower: 'cherry blossoms' });
-
-  // Read
-  const flowerInYoshinoCollection = await yoshino.get('mt_yoshino');
-  console.log(flowerInYoshinoCollection); // flowerInYoshinoCollection = { flower: 'cherry blossoms', _id: 'mt_yoshino' }
-
-  // Read all the documents in nara collection
-  const flowersInNaraCollection = await nara.allDocs({ include_docs: true });
-  console.dir(flowersInNaraCollection, { depth: 3 });
-  /* flowersInNaraCollection = 
-  {
-    total_rows: 2,
-    commit_sha: 'xxxxx_commit_sha_of_your_head_commit_xxxxx',
-    rows: [
-      {
-        id: 'nara_park',
-        file_sha: '7448ca2f7f79d6bb585421c6c29446acb97e4a8c',
-        doc: { flower: 'double cherry blossoms', _id: 'nara_park' }
-      },
-      {
-        id: 'tsukigase',
-        file_sha: '1241d69c4e9cd7a27f592affce94ec60d3b2207c',
-        doc: { flower: 'Japanese apricot', _id: 'tsukigase' }
-      }
-    ]
-  }
-  */
-
-  // Read one by using filepath
-  const flowerInYoshinoCollectionByPath = await gitDDB.get('yoshino/mt_yoshino');
-  console.log(flowerInYoshinoCollectionByPath); // flowerInYoshinoCollectionByPath = { flower: 'cherry blossoms', _id: 'yoshino/mt_yoshino' }
-
-  /*
-   * Actually, the collection is a sugar syntax of filepath.
-   * Both filepath (like PouchDB) and collection put the same file on the same location in a Git repository.
-   *
-   * Please check the generated files under examples/git-documentdb/db01/ and examples/git-documentdb/db02/ are the same.
-   * 
-   * Notice that API returns different _id values despite the same JSON document in a Git repository.
-   * 
-   * e.g) gitDDB.get({ _id: 'yoshino/mt_yoshino' }) returns { _id: 'yoshino/mt_yoshino', flower: 'cherry blossoms' }.
-   *      gitDDB.collection('yoshino').get({ _id: 'mt_yoshino' }) returns { _id: 'mt_yoshino', flower: 'cherry blossoms' }.
-   */
-
   await gitDDB.close();
 };
 gitddb_example();
