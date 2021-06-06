698584168c95755dfbd4df38ab8ea63a76819ee2
sosuisen <hidekazu.kubota@gmail.com>
2021-06-06 18:22:50 +0900

    fix: fix log message in sync example

--- examples/src/utils.ts
+++ examples/src/utils.ts
@@ -13,9 +13,9 @@ export function showChanges (syncResult: SyncResult, localLabel: string) {
     syncResult.action === 'resolve conflicts and push error'){
     syncResult.changes.local.forEach(file => {
       if (file.operation === 'insert')
-        console.log(' - insert ' + JSON.stringify(file.new.doc) + ' on ' + localLabel);
+        console.log(' - insert ' + JSON.stringify(file.new.doc) + ' into ' + localLabel);
       if (file.operation === 'delete')
-        console.log(' - delete ' + JSON.stringify(file.old.doc) + ' on ' + localLabel);
+        console.log(' - delete ' + JSON.stringify(file.old.doc) + ' from ' + localLabel);
       if(file.operation === 'update') {
         console.log(' - update from ' + JSON.stringify(file.old.doc));
         console.log('            to ' + JSON.stringify(file.new.doc) + ' on ' + localLabel);        
@@ -28,9 +28,9 @@ export function showChanges (syncResult: SyncResult, localLabel: string) {
     syncResult.action === 'resolve conflicts and push') {
     syncResult.changes.remote.forEach(file => {
       if (file.operation === 'insert')
-        console.log(' - insert ' + JSON.stringify(file.new.doc) + ' on GitHub');
+        console.log(' - insert ' + JSON.stringify(file.new.doc) + ' into GitHub');
       if (file.operation === 'delete')
-        console.log(' - delete ' + JSON.stringify(file.old.doc) + ' on GitHub');
+        console.log(' - delete ' + JSON.stringify(file.old.doc) + ' from GitHub');
       if(file.operation === 'update') {
         console.log(' - update from ' + JSON.stringify(file.old.doc));
         console.log('            to ' + JSON.stringify(file.new.doc) + ' on GitHub');        
