6030ab98c0d89abde5912f39d916847d9940435a
sosuisen <hidekazu.kubota@gmail.com>
2021-06-14 14:20:22 +0900

    test: pass open.test.ts, use isomorphic-git instead of NodeGit in open.test.ts

--- test/open.test.ts
+++ test/open.test.ts
@@ -9,7 +9,7 @@
 
 import path from 'path';
 import expect from 'expect';
-import nodegit from '@sosuisen/nodegit';
+import git from 'isomorphic-git';
 import { monotonicFactory } from 'ulid';
 import fs from 'fs-extra';
 import sinon from 'sinon';
@@ -17,20 +17,18 @@ import {
   CannotCreateDirectoryError,
   CannotCreateRepositoryError,
   CannotOpenRepositoryError,
-  InvalidWorkingDirectoryPathLengthError,
+  DatabaseClosingError,
   RepositoryNotFoundError,
-  UndefinedDatabaseNameError,
 } from '../src/error';
 import { generateDatabaseId, GitDocumentDB } from '../src/index';
-import { putWorker } from '../src/crud/put';
 import { DatabaseInfo, DatabaseOpenResult } from '../src/types';
 import {
   DATABASE_CREATOR,
   DATABASE_VERSION,
+  FIRST_COMMIT_MESSAGE,
   GIT_DOCUMENTDB_INFO_ID,
   JSON_EXT,
 } from '../src/const';
-import { Validator } from '../src/validator';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs_module = require('fs-extra');
@@ -41,6 +39,23 @@ const monoId = () => {
 };
 
 const localDir = `./test/database_open`;
+
+async function createDatabaseInfo (workingDir: string, info: string) {
+  const infoPath = path.resolve(workingDir, GIT_DOCUMENTDB_INFO_ID + JSON_EXT);
+  await fs.ensureDir(path.dirname(infoPath));
+  git.init({ fs, dir: workingDir, defaultBranch: 'main' });
+  await fs.writeFile(infoPath, info);
+  await git.add({ fs, dir: workingDir, filepath: GIT_DOCUMENTDB_INFO_ID + JSON_EXT });
+  await git.commit({
+    fs,
+    dir: workingDir,
+    author: {
+      name: 'test',
+      email: 'text@example.com',
+    },
+    message: FIRST_COMMIT_MESSAGE,
+  });
+}
 
 // Use sandbox to restore stub and spy in parallel mocha tests
 let sandbox: sinon.SinonSandbox;
@@ -59,89 +74,33 @@ after(() => {
 });
 
 describe('<index>', () => {
-  describe('GitDocumentDB#constructor', () => {
-    it('new', () => {
-      expect(() => {
-        // eslint-disable-next-line no-new
-        new GitDocumentDB({ dbName: 'db', localDir: 'C:\\dir01\\dir02' });
-      }).not.toThrowError();
-    });
-
-    it('throws UndefinedDatabaseNameError.', () => {
-      // Code must be wrapped by () => {} to test exception
-      // https://jestjs.io/docs/en/expect#tothrowerror
-      expect(() => {
-        /* eslint-disable-next-line no-new */ // @ts-ignore
-        new GitDocumentDB({});
-      }).toBeInstanceOf(UndefinedDatabaseNameError);
-    });
-
-    it('throws InvalidWorkingDirectoryPathLengthError when tries to create a long name repository.', async () => {
-      const maxWorkingDirLen = Validator.maxWorkingDirectoryLength();
-      let dbName = 'tmp';
-      const workingDirectory = path.resolve(localDir, dbName);
-      for (let i = 0; i < maxWorkingDirLen - workingDirectory.length; i++) {
-        dbName += '0';
-      }
-
-      // Code must be wrapped by () => {} to test exception
-      // https://jestjs.io/docs/en/expect#tothrowerror
-      let gitddb: GitDocumentDB;
-      expect(() => {
-        gitddb = new GitDocumentDB({
-          dbName,
-          localDir,
-        });
-      }).not.toThrowError();
-      // @ts-ignore
-      if (gitddb !== undefined) {
-        await gitddb.destroy();
-      }
-
-      dbName += '0';
-      expect(() => {
-        // eslint-disable-next-line no-new
-        new GitDocumentDB({
-          dbName,
-          localDir,
-        });
-      }).toBeInstanceOf(InvalidWorkingDirectoryPathLengthError);
-    });
-
-    it('throws InvalidWorkingDirectoryPathLengthError when working directory path is too long.', () => {
-      expect(() => {
-        /* eslint-disable-next-line no-new */ // @ts-ignore
-        new GitDocumentDB({
-          dbName:
-            '0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789',
-          localDir:
-            '0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789',
-        });
-      }).toBeInstanceOf(InvalidWorkingDirectoryPathLengthError);
-    });
-  });
-
   describe('create', () => {
-    const readonlyDir = './test/_readonly/';
+    it('throws DatabaseClosingError.', async () => {
+      const dbName = monoId();
+      const gitDDB = new GitDocumentDB({
+        dbName,
+        localDir,
+      });
+      await gitDDB.open();
+
+      for (let i = 0; i < 100; i++) {
+        gitDDB.put({ _id: i.toString(), name: i.toString() }).catch(() => {});
+      }
+      // Call close() without await
+      gitDDB.close().catch(() => {});
+      await expect(gitDDB.open()).rejects.toBeInstanceOf(DatabaseClosingError);
+    });
 
     it('throws CannotCreateDirectoryError when tries to create a new repository on a readonly filesystem.', async () => {
       const dbName = monoId();
       // Windows does not support permission option of fs.ensureDir(). Use stub.
-      if (process.platform === 'win32') {
-        const stubEnsureDir = sandbox.stub(fs_module, 'ensureDir');
-        stubEnsureDir.rejects();
-      }
-      else {
-        if (!fs.existsSync('test')) {
-          await fs.mkdir('test');
-        }
-        await fs.ensureDir(readonlyDir, { mode: 0o400 }).catch((err: Error) => {
-          console.error(err);
-        });
-      }
+      const stubEnsureDir = sandbox.stub(fs_module, 'ensureDir');
+      stubEnsureDir.rejects();
+
+      const readonlyDir = './test/_readonly/';
       const gitDDB: GitDocumentDB = new GitDocumentDB({
         dbName,
-        localDir: readonlyDir + 'database',
+        localDir,
       });
       // You don't have permission
       await expect(gitDDB.open()).rejects.toBeInstanceOf(CannotCreateDirectoryError);
@@ -154,9 +113,10 @@ describe('<index>', () => {
       const dbName = monoId();
       const stubEnsureDir = sandbox.stub(fs_module, 'ensureDir');
       stubEnsureDir.onFirstCall().resolves().onSecondCall().rejects();
+
       const gitDDB: GitDocumentDB = new GitDocumentDB({
         dbName,
-        localDir: readonlyDir + 'database',
+        localDir,
       });
       // You don't have permission
       await expect(gitDDB.open()).rejects.toBeInstanceOf(CannotCreateRepositoryError);
@@ -213,193 +173,172 @@ describe('<index>', () => {
     });
   });
 
-  it('throws RepositoryNotFoundError.', async () => {
-    const dbName = monoId();
-    const gitDDB: GitDocumentDB = new GitDocumentDB({
-      dbName,
-      localDir,
-    });
-    await expect(gitDDB.open({ createIfNotExists: false })).rejects.toBeInstanceOf(
-      RepositoryNotFoundError
-    );
-  });
-
-  it('throws CannotOpenRepositoryError.', async () => {
-    const dbName = monoId();
-    const gitDDB: GitDocumentDB = new GitDocumentDB({
-      dbName,
-      localDir,
-    });
-    // Create empty .git directory
-    await fs.ensureDir(gitDDB.workingDir() + '/.git/');
-    await expect(gitDDB.open()).rejects.toBeInstanceOf(CannotOpenRepositoryError);
-  });
-
-  it('opens an existing repository.', async () => {
-    const dbName = monoId();
-    const gitDDB: GitDocumentDB = new GitDocumentDB({
-      dbName,
-      localDir,
+  describe('open', () => {
+    it('throws RepositoryNotFoundError.', async () => {
+      const dbName = monoId();
+      const gitDDB: GitDocumentDB = new GitDocumentDB({
+        dbName,
+        localDir,
+      });
+      await expect(gitDDB.open({ createIfNotExists: false })).rejects.toBeInstanceOf(
+        RepositoryNotFoundError
+      );
     });
 
-    // Create db
-    const oldResult = (await gitDDB.open()) as DatabaseInfo;
-
-    // Close created db
-    await expect(gitDDB.close()).resolves.toBeUndefined();
-
-    // Open existing db
-    const dbOpenResult = await gitDDB.open();
-    expect(dbOpenResult).toEqual({
-      dbId: oldResult.dbId,
-      creator: DATABASE_CREATOR,
-      version: DATABASE_VERSION,
-      isNew: false,
-      isCreatedByGitddb: true,
-      isValidVersion: true,
-    });
-    expect(gitDDB.isOpened()).toBeTruthy();
-
-    // Destroy() closes db automatically
-    await expect(gitDDB.destroy()).resolves.toEqual({ ok: true });
-    expect(gitDDB.isOpened()).toBeFalsy();
-  });
-
-  it('opens a repository created by another app.', async () => {
-    const dbName = monoId();
-    const gitDDB = new GitDocumentDB({
-      dbName,
-      localDir,
-    });
-    await fs.ensureDir(gitDDB.workingDir());
-
-    // Create empty repository
-    await nodegit.Repository.init(gitDDB.workingDir(), 0).catch(err => {
-      return Promise.reject(err);
-    });
-    await gitDDB.open();
-    // put another app
-    const creator = 'Another App';
-    await putWorker(
-      gitDDB,
-      GIT_DOCUMENTDB_INFO_ID + JSON_EXT,
-      JSON.stringify({
-        creator,
-      }),
-      'first commit'
-    );
-    await gitDDB.close();
-
-    const dbOpenResult = await gitDDB.open();
-    expect(dbOpenResult).toEqual({
-      dbId: (dbOpenResult as DatabaseInfo).dbId,
-      creator,
-      version: '',
-      isNew: false,
-      isCreatedByGitddb: false,
-      isValidVersion: false,
-    });
-    await gitDDB.destroy();
-  });
-
-  it('opens a repository created by another version.', async () => {
-    const dbName = monoId();
-    const gitDDB = new GitDocumentDB({
-      dbName,
-      localDir,
+    it('throws CannotOpenRepositoryError.', async () => {
+      const dbName = monoId();
+      const gitDDB: GitDocumentDB = new GitDocumentDB({
+        dbName,
+        localDir,
+      });
+      // Create empty .git directory
+      await fs.ensureDir(gitDDB.workingDir() + '/.git/');
+      await expect(gitDDB.open()).rejects.toBeInstanceOf(CannotOpenRepositoryError);
     });
 
-    // Create empty repository
-    await nodegit.Repository.init(gitDDB.workingDir(), 0).catch(err => {
-      return Promise.reject(err);
-    });
-    await gitDDB.open();
-    // First commit with another db version
-    await putWorker(
-      gitDDB,
-      GIT_DOCUMENTDB_INFO_ID + JSON_EXT,
-      JSON.stringify({
-        dbId: generateDatabaseId(),
-        creator: DATABASE_CREATOR,
-        version: '0.01',
-      }),
-      'first commit'
-    );
-    await gitDDB.close();
+    it('opens an existing repository.', async () => {
+      const dbName = monoId();
+      const gitDDB: GitDocumentDB = new GitDocumentDB({
+        dbName,
+        localDir,
+      });
 
-    const dbOpenResult = await gitDDB.open();
-    expect(dbOpenResult).toEqual({
-      dbId: (dbOpenResult as DatabaseInfo).dbId,
-      creator: DATABASE_CREATOR,
-      version: '0.01',
-      isNew: false,
-      isCreatedByGitddb: true,
-      isValidVersion: false,
-    });
-    await gitDDB.destroy();
-  });
+      // Create db
+      const oldResult = (await gitDDB.open()) as DatabaseInfo;
 
-  it('returns new dbId when opens db without dbId.', async () => {
-    const dbName = monoId();
-    const gitDDB = new GitDocumentDB({
-      dbName,
-      localDir,
-    });
-    // Create empty repository
-    await nodegit.Repository.init(gitDDB.workingDir(), 0).catch(err => {
-      return Promise.reject(err);
-    });
+      // Close created db
+      await expect(gitDDB.close()).resolves.toBeUndefined();
 
-    await gitDDB.open();
-
-    const prevDbId = gitDDB.dbId();
-    // First commit with another db version
-    await putWorker(
-      gitDDB,
-      GIT_DOCUMENTDB_INFO_ID + JSON_EXT,
-      JSON.stringify({
-        dbId: '',
+      // Open existing db
+      const dbOpenResult = await gitDDB.open();
+      expect(dbOpenResult).toEqual({
+        dbId: oldResult.dbId,
         creator: DATABASE_CREATOR,
         version: DATABASE_VERSION,
-      }),
-      'first commit'
-    );
-    await gitDDB.close();
+        isNew: false,
+        isCreatedByGitddb: true,
+        isValidVersion: true,
+      });
+      expect(gitDDB.isOpened()).toBeTruthy();
 
-    const dbOpenResult = await gitDDB.open();
-    const newDbId = (dbOpenResult as DatabaseInfo).dbId;
-
-    expect(newDbId).not.toBe(prevDbId);
-    expect(dbOpenResult).toEqual({
-      dbId: newDbId,
-      creator: DATABASE_CREATOR,
-      version: DATABASE_VERSION,
-      isNew: false,
-      isCreatedByGitddb: true,
-      isValidVersion: true,
-    });
-  });
-
-  it('opens db twice.', async () => {
-    const dbName = monoId();
-    const gitDDB: GitDocumentDB = new GitDocumentDB({
-      dbName,
-      localDir,
+      // Destroy() closes db automatically
+      await expect(gitDDB.destroy()).resolves.toEqual({ ok: true });
+      expect(gitDDB.isOpened()).toBeFalsy();
     });
 
-    // Create db
-    await gitDDB.open();
+    it('opens a repository created by another app.', async () => {
+      const dbName = monoId();
+      const gitDDB = new GitDocumentDB({
+        dbName,
+        localDir,
+      });
+      await fs.ensureDir(gitDDB.workingDir());
 
-    const dbOpenResult = await gitDDB.open();
-    const newDbId = (dbOpenResult as DatabaseInfo).dbId;
-    await expect(gitDDB.open()).resolves.toMatchObject({
-      dbId: newDbId,
-      creator: DATABASE_CREATOR,
-      version: DATABASE_VERSION,
-      isNew: false,
-      isCreatedByGitddb: true,
-      isValidVersion: true,
+      // put another app
+      const creator = 'Another App';
+      await createDatabaseInfo(
+        gitDDB.workingDir(),
+        JSON.stringify({
+          creator,
+        })
+      );
+
+      const dbOpenResult = await gitDDB.open();
+      const newDbId = (dbOpenResult as DatabaseInfo).dbId;
+      expect(newDbId).toMatch(/^[\dA-HJKMNP-TV-Z]{26}$/);
+
+      expect(dbOpenResult).toEqual({
+        dbId: newDbId,
+        creator,
+        version: '',
+        isNew: false,
+        isCreatedByGitddb: false,
+        isValidVersion: false,
+      });
+      await gitDDB.destroy();
     });
-    await gitDDB.destroy();
+
+    it('opens a repository created by another version.', async () => {
+      const dbName = monoId();
+      const gitDDB = new GitDocumentDB({
+        dbName,
+        localDir,
+      });
+
+      const dbId = generateDatabaseId();
+      await createDatabaseInfo(
+        gitDDB.workingDir(),
+        JSON.stringify({
+          dbId,
+          creator: DATABASE_CREATOR,
+          version: '0.01',
+        })
+      );
+
+      const dbOpenResult = await gitDDB.open();
+
+      expect(dbOpenResult).toEqual({
+        dbId,
+        creator: DATABASE_CREATOR,
+        version: '0.01',
+        isNew: false,
+        isCreatedByGitddb: true,
+        isValidVersion: false,
+      });
+      await gitDDB.destroy();
+    });
+
+    it('returns new dbId when opens db which does not have dbId.', async () => {
+      const dbName = monoId();
+      const gitDDB = new GitDocumentDB({
+        dbName,
+        localDir,
+      });
+
+      await createDatabaseInfo(
+        gitDDB.workingDir(),
+        JSON.stringify({
+          dbId: '',
+          creator: DATABASE_CREATOR,
+          version: DATABASE_VERSION,
+        })
+      );
+
+      const dbOpenResult = await gitDDB.open();
+      const newDbId = (dbOpenResult as DatabaseInfo).dbId;
+      expect(newDbId).toMatch(/^[\dA-HJKMNP-TV-Z]{26}$/);
+
+      expect(dbOpenResult).toEqual({
+        dbId: newDbId,
+        creator: DATABASE_CREATOR,
+        version: DATABASE_VERSION,
+        isNew: false,
+        isCreatedByGitddb: true,
+        isValidVersion: true,
+      });
+    });
+
+    it('opens db twice.', async () => {
+      const dbName = monoId();
+      const gitDDB: GitDocumentDB = new GitDocumentDB({
+        dbName,
+        localDir,
+      });
+
+      // Create db
+      await gitDDB.open();
+
+      const dbOpenResult = await gitDDB.open();
+      const newDbId = (dbOpenResult as DatabaseInfo).dbId;
+      await expect(gitDDB.open()).resolves.toMatchObject({
+        dbId: newDbId,
+        creator: DATABASE_CREATOR,
+        version: DATABASE_VERSION,
+        isNew: false,
+        isCreatedByGitddb: true,
+        isValidVersion: true,
+      });
+      await gitDDB.destroy();
+    });
   });
 });
