63a437d20f14c9f6b9c3cfd9ab853c047d2c5375
sosuisen <hidekazu.kubota@gmail.com>
2021-06-19 05:05:55 +0900

    test: add tests for findImpl with collectionPath

--- test/crud/find.test.ts
+++ test/crud/find.test.ts
@@ -168,10 +168,6 @@ describe('<crud/find> find()', () => {
       localDir,
     });
 
-    await expect(findImpl(gitDDB, '', true, false)).rejects.toThrowError(
-      RepositoryNotOpenError
-    );
-
     await gitDDB.open();
 
     await expect(findImpl(gitDDB, '', true, false)).resolves.toEqual([]);
@@ -516,5 +512,458 @@ describe('<crud/find> find()', () => {
     });
   });
 
-  describe('with collectionPath', () => {});
+  describe('with collectionPath', () => {
+    it('returns empty', async () => {
+      const dbName = monoId();
+
+      const gitDDB: GitDocumentDB = new GitDocumentDB({
+        dbName,
+        localDir,
+      });
+
+      await gitDDB.open();
+
+      await expect(findImpl(gitDDB, 'col01', true, false)).resolves.toEqual([]);
+
+      await gitDDB.destroy();
+    });
+
+    it('returns entries by ascending alphabetic order', async () => {
+      const dbName = monoId();
+
+      const gitDDB: GitDocumentDB = new GitDocumentDB({
+        dbName,
+        localDir,
+      });
+
+      await expect(findImpl(gitDDB, '', true, false)).rejects.toThrowError(
+        RepositoryNotOpenError
+      );
+
+      await gitDDB.open();
+
+      const colPath = 'col01/';
+      const json_b = { _id: colPath + _id_b, name: name_b };
+      const json_a = { _id: colPath + _id_a, name: name_a };
+      const json_1 = { _id: colPath + _id_1, name: name_1 };
+      const json_c = { _id: colPath + _id_c, name: name_c };
+
+      const json_b_ = { _id: _id_b, name: name_b };
+      const json_a_ = { _id: _id_a, name: name_a };
+      const json_1_ = { _id: _id_1, name: name_1 };
+      const json_c_ = { _id: _id_c, name: name_c };
+
+      await addOneData(gitDDB, colPath + _id_b + JSON_EXT, toSortedJSONString(json_b));
+      await addOneData(gitDDB, colPath + _id_a + JSON_EXT, toSortedJSONString(json_a));
+      await addOneData(gitDDB, colPath + _id_1 + JSON_EXT, toSortedJSONString(json_1));
+      await addOneData(gitDDB, colPath + _id_c + JSON_EXT, toSortedJSONString(json_c));
+
+      await expect(findImpl(gitDDB, colPath, true, false)).resolves.toEqual([
+        json_1_,
+        json_a_,
+        json_b_,
+        json_c_,
+      ]);
+
+      await gitDDB.destroy();
+    });
+
+    it('returns docs by breadth-first search (recursive)', async () => {
+      const dbName = monoId();
+      const gitDDB: GitDocumentDB = new GitDocumentDB({
+        dbName,
+        localDir,
+      });
+      await gitDDB.open();
+
+      const colPath = 'col01/';
+      const json_b = { _id: colPath + _id_b, name: name_b };
+      const json_a = { _id: colPath + _id_a, name: name_a };
+      const json_d = { _id: colPath + _id_d, name: name_d };
+      const json_c01 = { _id: colPath + _id_c01, name: name_c01 };
+      const json_c02 = { _id: colPath + _id_c02, name: name_c02 };
+
+      const json_b_ = { _id: _id_b, name: name_b };
+      const json_a_ = { _id: _id_a, name: name_a };
+      const json_d_ = { _id: _id_d, name: name_d };
+      const json_c01_ = { _id: _id_c01, name: name_c01 };
+      const json_c02_ = { _id: _id_c02, name: name_c02 };
+
+      await addOneData(gitDDB, colPath + _id_b + JSON_EXT, toSortedJSONString(json_b));
+      await addOneData(gitDDB, colPath + _id_a + JSON_EXT, toSortedJSONString(json_a));
+      await addOneData(gitDDB, colPath + _id_d + JSON_EXT, toSortedJSONString(json_d));
+      await addOneData(gitDDB, colPath + _id_c01 + JSON_EXT, toSortedJSONString(json_c01));
+      await addOneData(gitDDB, colPath + _id_c02 + JSON_EXT, toSortedJSONString(json_c02));
+
+      await expect(findImpl(gitDDB, colPath, true, false)).resolves.toEqual([
+        json_a_,
+        json_b_,
+        json_c01_,
+        json_c02_,
+        json_d_,
+      ]);
+
+      await gitDDB.destroy();
+    });
+
+    describe('and prefix search', () => {
+      it('gets from directory', async () => {
+        const dbName = monoId();
+        const gitDDB: GitDocumentDB = new GitDocumentDB({
+          dbName,
+          localDir,
+        });
+        await gitDDB.open();
+
+        const colPath = 'col01/';
+        const json_b = { _id: colPath + _id_b, name: name_b };
+        const json_a = { _id: colPath + _id_a, name: name_a };
+        const json_d = { _id: colPath + _id_d, name: name_d };
+        const json_c000 = { _id: colPath + _id_c000, name: name_c000 };
+        const json_c001 = { _id: colPath + _id_c001, name: name_c001 };
+        const json_c01 = { _id: colPath + _id_c01, name: name_c01 };
+        const json_c02 = { _id: colPath + _id_c02, name: name_c02 };
+
+        const json_b_ = { _id: _id_b, name: name_b };
+        const json_a_ = { _id: _id_a, name: name_a };
+        const json_d_ = { _id: _id_d, name: name_d };
+        const json_c000_ = { _id: _id_c000, name: name_c000 };
+        const json_c001_ = { _id: _id_c001, name: name_c001 };
+        const json_c01_ = { _id: _id_c01, name: name_c01 };
+        const json_c02_ = { _id: _id_c02, name: name_c02 };
+
+        await addOneData(gitDDB, colPath + _id_b + JSON_EXT, toSortedJSONString(json_b));
+        await addOneData(gitDDB, colPath + _id_a + JSON_EXT, toSortedJSONString(json_a));
+        await addOneData(gitDDB, colPath + _id_d + JSON_EXT, toSortedJSONString(json_d));
+        await addOneData(
+          gitDDB,
+          colPath + _id_c000 + JSON_EXT,
+          toSortedJSONString(json_c000)
+        );
+        await addOneData(
+          gitDDB,
+          colPath + _id_c001 + JSON_EXT,
+          toSortedJSONString(json_c001)
+        );
+        await addOneData(
+          gitDDB,
+          colPath + _id_c01 + JSON_EXT,
+          toSortedJSONString(json_c01)
+        );
+        await addOneData(
+          gitDDB,
+          colPath + _id_c02 + JSON_EXT,
+          toSortedJSONString(json_c02)
+        );
+
+        const prefix = 'citrus/';
+
+        await expect(findImpl(gitDDB, colPath, true, false, { prefix })).resolves.toEqual([
+          json_c01_,
+          json_c02_,
+        ]);
+
+        await gitDDB.destroy();
+      });
+
+      it('gets only from top directory', async () => {
+        const dbName = monoId();
+        const gitDDB: GitDocumentDB = new GitDocumentDB({
+          dbName,
+          localDir,
+        });
+        await gitDDB.open();
+
+        const colPath = 'col01/';
+        const json_b = { _id: colPath + _id_b, name: name_b };
+        const json_a = { _id: colPath + _id_a, name: name_a };
+        const json_d = { _id: colPath + _id_d, name: name_d };
+        const json_c000 = { _id: colPath + _id_c000, name: name_c000 };
+        const json_c001 = { _id: colPath + _id_c001, name: name_c001 };
+        const json_c01 = { _id: colPath + _id_c01, name: name_c01 };
+        const json_c02 = { _id: colPath + _id_c02, name: name_c02 };
+
+        const json_b_ = { _id: _id_b, name: name_b };
+        const json_a_ = { _id: _id_a, name: name_a };
+        const json_d_ = { _id: _id_d, name: name_d };
+        const json_c000_ = { _id: _id_c000, name: name_c000 };
+        const json_c001_ = { _id: _id_c001, name: name_c001 };
+        const json_c01_ = { _id: _id_c01, name: name_c01 };
+        const json_c02_ = { _id: _id_c02, name: name_c02 };
+
+        await addOneData(gitDDB, colPath + _id_b + JSON_EXT, toSortedJSONString(json_b));
+        await addOneData(gitDDB, colPath + _id_a + JSON_EXT, toSortedJSONString(json_a));
+        await addOneData(gitDDB, colPath + _id_d + JSON_EXT, toSortedJSONString(json_d));
+        await addOneData(
+          gitDDB,
+          colPath + _id_c000 + JSON_EXT,
+          toSortedJSONString(json_c000)
+        );
+        await addOneData(
+          gitDDB,
+          colPath + _id_c001 + JSON_EXT,
+          toSortedJSONString(json_c001)
+        );
+        await addOneData(
+          gitDDB,
+          colPath + _id_c01 + JSON_EXT,
+          toSortedJSONString(json_c01)
+        );
+        await addOneData(
+          gitDDB,
+          colPath + _id_c02 + JSON_EXT,
+          toSortedJSONString(json_c02)
+        );
+
+        const prefix = 'cit';
+
+        await expect(
+          findImpl(gitDDB, colPath, true, false, { prefix, recursive: false })
+        ).resolves.toEqual([json_c000_, json_c001_]);
+
+        await gitDDB.destroy();
+      });
+
+      it('gets from parent directory and child directory', async () => {
+        const dbName = monoId();
+        const gitDDB: GitDocumentDB = new GitDocumentDB({
+          dbName,
+          localDir,
+        });
+        await gitDDB.open();
+
+        const colPath = 'col01/';
+        const json_b = { _id: colPath + _id_b, name: name_b };
+        const json_a = { _id: colPath + _id_a, name: name_a };
+        const json_d = { _id: colPath + _id_d, name: name_d };
+        const json_c000 = { _id: colPath + _id_c000, name: name_c000 };
+        const json_c001 = { _id: colPath + _id_c001, name: name_c001 };
+        const json_c01 = { _id: colPath + _id_c01, name: name_c01 };
+        const json_c02 = { _id: colPath + _id_c02, name: name_c02 };
+
+        const json_b_ = { _id: _id_b, name: name_b };
+        const json_a_ = { _id: _id_a, name: name_a };
+        const json_d_ = { _id: _id_d, name: name_d };
+        const json_c000_ = { _id: _id_c000, name: name_c000 };
+        const json_c001_ = { _id: _id_c001, name: name_c001 };
+        const json_c01_ = { _id: _id_c01, name: name_c01 };
+        const json_c02_ = { _id: _id_c02, name: name_c02 };
+
+        await addOneData(gitDDB, colPath + _id_b + JSON_EXT, toSortedJSONString(json_b));
+        await addOneData(gitDDB, colPath + _id_a + JSON_EXT, toSortedJSONString(json_a));
+        await addOneData(gitDDB, colPath + _id_d + JSON_EXT, toSortedJSONString(json_d));
+        await addOneData(
+          gitDDB,
+          colPath + _id_c000 + JSON_EXT,
+          toSortedJSONString(json_c000)
+        );
+        await addOneData(
+          gitDDB,
+          colPath + _id_c001 + JSON_EXT,
+          toSortedJSONString(json_c001)
+        );
+        await addOneData(
+          gitDDB,
+          colPath + _id_c01 + JSON_EXT,
+          toSortedJSONString(json_c01)
+        );
+        await addOneData(
+          gitDDB,
+          colPath + _id_c02 + JSON_EXT,
+          toSortedJSONString(json_c02)
+        );
+
+        const prefix = 'citrus';
+
+        await expect(findImpl(gitDDB, colPath, true, false, { prefix })).resolves.toEqual([
+          json_c000_,
+          json_c001_,
+          json_c01_,
+          json_c02_,
+        ]);
+
+        await gitDDB.destroy();
+      });
+
+      it('gets from a sub directory', async () => {
+        const dbName = monoId();
+        const gitDDB: GitDocumentDB = new GitDocumentDB({
+          dbName,
+          localDir,
+        });
+        await gitDDB.open();
+
+        const colPath = 'col01/';
+        const json_b = { _id: colPath + _id_b, name: name_b };
+        const json_a = { _id: colPath + _id_a, name: name_a };
+        const json_d = { _id: colPath + _id_d, name: name_d };
+        const json_c000 = { _id: colPath + _id_c000, name: name_c000 };
+        const json_c001 = { _id: colPath + _id_c001, name: name_c001 };
+        const json_c01 = { _id: colPath + _id_c01, name: name_c01 };
+        const json_c02 = { _id: colPath + _id_c02, name: name_c02 };
+
+        const json_b_ = { _id: _id_b, name: name_b };
+        const json_a_ = { _id: _id_a, name: name_a };
+        const json_d_ = { _id: _id_d, name: name_d };
+        const json_c000_ = { _id: _id_c000, name: name_c000 };
+        const json_c001_ = { _id: _id_c001, name: name_c001 };
+        const json_c01_ = { _id: _id_c01, name: name_c01 };
+        const json_c02_ = { _id: _id_c02, name: name_c02 };
+
+        await addOneData(gitDDB, colPath + _id_b + JSON_EXT, toSortedJSONString(json_b));
+        await addOneData(gitDDB, colPath + _id_a + JSON_EXT, toSortedJSONString(json_a));
+        await addOneData(gitDDB, colPath + _id_d + JSON_EXT, toSortedJSONString(json_d));
+        await addOneData(
+          gitDDB,
+          colPath + _id_c000 + JSON_EXT,
+          toSortedJSONString(json_c000)
+        );
+        await addOneData(
+          gitDDB,
+          colPath + _id_c001 + JSON_EXT,
+          toSortedJSONString(json_c001)
+        );
+        await addOneData(
+          gitDDB,
+          colPath + _id_c01 + JSON_EXT,
+          toSortedJSONString(json_c01)
+        );
+        await addOneData(
+          gitDDB,
+          colPath + _id_c02 + JSON_EXT,
+          toSortedJSONString(json_c02)
+        );
+
+        const prefix = 'citrus/y';
+
+        await expect(findImpl(gitDDB, colPath, true, false, { prefix })).resolves.toEqual([
+          json_c02_,
+        ]);
+
+        await gitDDB.destroy();
+      });
+
+      it('returns no entry when prefix does not match', async () => {
+        const dbName = monoId();
+        const gitDDB: GitDocumentDB = new GitDocumentDB({
+          dbName,
+          localDir,
+        });
+        await gitDDB.open();
+
+        const colPath = 'col01/';
+        const json_b = { _id: colPath + _id_b, name: name_b };
+        const json_a = { _id: colPath + _id_a, name: name_a };
+        const json_d = { _id: colPath + _id_d, name: name_d };
+        const json_c000 = { _id: colPath + _id_c000, name: name_c000 };
+        const json_c001 = { _id: colPath + _id_c001, name: name_c001 };
+        const json_c01 = { _id: colPath + _id_c01, name: name_c01 };
+        const json_c02 = { _id: colPath + _id_c02, name: name_c02 };
+
+        const json_b_ = { _id: _id_b, name: name_b };
+        const json_a_ = { _id: _id_a, name: name_a };
+        const json_d_ = { _id: _id_d, name: name_d };
+        const json_c000_ = { _id: _id_c000, name: name_c000 };
+        const json_c001_ = { _id: _id_c001, name: name_c001 };
+        const json_c01_ = { _id: _id_c01, name: name_c01 };
+        const json_c02_ = { _id: _id_c02, name: name_c02 };
+
+        await addOneData(gitDDB, colPath + _id_b + JSON_EXT, toSortedJSONString(json_b));
+        await addOneData(gitDDB, colPath + _id_a + JSON_EXT, toSortedJSONString(json_a));
+        await addOneData(gitDDB, colPath + _id_d + JSON_EXT, toSortedJSONString(json_d));
+        await addOneData(
+          gitDDB,
+          colPath + _id_c000 + JSON_EXT,
+          toSortedJSONString(json_c000)
+        );
+        await addOneData(
+          gitDDB,
+          colPath + _id_c001 + JSON_EXT,
+          toSortedJSONString(json_c001)
+        );
+        await addOneData(
+          gitDDB,
+          colPath + _id_c01 + JSON_EXT,
+          toSortedJSONString(json_c01)
+        );
+        await addOneData(
+          gitDDB,
+          colPath + _id_c02 + JSON_EXT,
+          toSortedJSONString(json_c02)
+        );
+
+        const prefix = 'not_exist/';
+
+        await expect(findImpl(gitDDB, colPath, true, false, { prefix })).resolves.toEqual(
+          []
+        );
+
+        await gitDDB.destroy();
+      });
+
+      it('gets from deep directory', async () => {
+        const dbName = monoId();
+        const gitDDB: GitDocumentDB = new GitDocumentDB({
+          dbName,
+          localDir,
+        });
+        await gitDDB.open();
+
+        const colPath = 'col01/';
+        const json_p = { _id: colPath + _id_p, name: name_p };
+        const json_b = { _id: colPath + _id_b, name: name_b };
+        const json_a = { _id: colPath + _id_a, name: name_a };
+        const json_d = { _id: colPath + _id_d, name: name_d };
+        const json_c000 = { _id: colPath + _id_c000, name: name_c000 };
+        const json_c001 = { _id: colPath + _id_c001, name: name_c001 };
+        const json_c01 = { _id: colPath + _id_c01, name: name_c01 };
+        const json_c02 = { _id: colPath + _id_c02, name: name_c02 };
+
+        const json_p_ = { _id: _id_p, name: name_p };
+        const json_b_ = { _id: _id_b, name: name_b };
+        const json_a_ = { _id: _id_a, name: name_a };
+        const json_d_ = { _id: _id_d, name: name_d };
+        const json_c000_ = { _id: _id_c000, name: name_c000 };
+        const json_c001_ = { _id: _id_c001, name: name_c001 };
+        const json_c01_ = { _id: _id_c01, name: name_c01 };
+        const json_c02_ = { _id: _id_c02, name: name_c02 };
+
+        await addOneData(gitDDB, colPath + _id_p + JSON_EXT, toSortedJSONString(json_p));
+
+        await addOneData(gitDDB, colPath + _id_b + JSON_EXT, toSortedJSONString(json_b));
+        await addOneData(gitDDB, colPath + _id_a + JSON_EXT, toSortedJSONString(json_a));
+        await addOneData(gitDDB, colPath + _id_d + JSON_EXT, toSortedJSONString(json_d));
+        await addOneData(
+          gitDDB,
+          colPath + _id_c000 + JSON_EXT,
+          toSortedJSONString(json_c000)
+        );
+        await addOneData(
+          gitDDB,
+          colPath + _id_c001 + JSON_EXT,
+          toSortedJSONString(json_c001)
+        );
+        await addOneData(
+          gitDDB,
+          colPath + _id_c01 + JSON_EXT,
+          toSortedJSONString(json_c01)
+        );
+        await addOneData(
+          gitDDB,
+          colPath + _id_c02 + JSON_EXT,
+          toSortedJSONString(json_c02)
+        );
+
+        await expect(
+          findImpl(gitDDB, colPath, true, false, { prefix: 'pear/Japan' })
+        ).resolves.toEqual([json_p_]);
+
+        await expect(
+          findImpl(gitDDB, colPath, true, false, { prefix: 'pear' })
+        ).resolves.toEqual([json_p_]);
+
+        await gitDDB.destroy();
+      });
+    });
+  });
 });
