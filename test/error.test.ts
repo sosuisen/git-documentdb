cdf1902a941c916c696e132a109c992e1bd18d1a
sosuisen <hidekazu.kubota@gmail.com>
2021-06-19 16:38:43 +0900

    test: fix tests for error

--- test/error.test.ts
+++ test/error.test.ts
@@ -6,6 +6,7 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
+import expect from 'expect';
 import { InvalidCollectionPathError } from '../src/error';
 
 describe('<error>', () => {
