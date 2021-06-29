22812b55a39342ad2b3b1b39ad9cf31bdae02a28
sosuisen <hidekazu.kubota@gmail.com>
2021-06-30 02:30:00 +0900

    fix: sort const.ts in alphabetical order

--- src/const.ts
+++ src/const.ts
@@ -17,15 +17,11 @@ export const DATABASE_VERSION = '1.0';
 /**
  * @public
  */
-export const GIT_DOCUMENTDB_METADATA_DIR = '.gitddb';
+export const DEFAULT_COMBINE_DB_STRATEGY = 'combine-head-with-theirs';
 /**
  * @public
  */
-export const GIT_DOCUMENTDB_INFO_ID = '.gitddb/info';
-/**
- * @public
- */
-export const GIT_DOCUMENTDB_APP_INFO_ID = '.gitddb/app';
+export const DEFAULT_CONFLICT_RESOLUTION_STRATEGY = 'ours-diff';
 /**
  * @public
  */
@@ -34,25 +30,15 @@ export const DEFAULT_LOCAL_DIR = './git-documentdb';
 /**
  * @public
  */
-export const FIRST_COMMIT_MESSAGE = 'first commit';
-/**
- * @public
- */
-export const SET_DATABASE_ID_MESSAGE = 'set database id';
-/**
- * @public
- */
-export const PUT_APP_INFO_MESSAGE = 'put appinfo';
-
-/**
- * @public
- */
 export const DEFAULT_LOG_LEVEL = 'info';
-
 /**
  * @public
  */
-export const MAX_FILE_PATH_LENGTH = 255;
+export const DEFAULT_SYNC_INTERVAL = 30000;
+/**
+ * @public
+ */
+export const DUPLICATED_FILE_POSTFIX = '-from-';
 /**
  * @public
  */
@@ -60,12 +46,31 @@ export const FILE_REMOVE_TIMEOUT = 7000;
 /**
  * @public
  */
-export const SHORT_SHA_LENGTH = 7;
-
+export const FIRST_COMMIT_MESSAGE = 'first commit';
 /**
  * @public
  */
-export const NETWORK_TIMEOUT = 7000;
+export const GIT_DOCUMENTDB_APP_INFO_ID = '.gitddb/app';
+/**
+ * @public
+ */
+export const GIT_DOCUMENTDB_INFO_ID = '.gitddb/info';
+/**
+ * @public
+ */
+export const GIT_DOCUMENTDB_METADATA_DIR = '.gitddb';
+/**
+ * @public
+ */
+export const JSON_EXT = '.json';
+/**
+ * @public
+ */
+export const MAX_FILE_PATH_LENGTH = 255;
+/**
+ * @public
+ */
+export const MINIMUM_SYNC_INTERVAL = 3000;
 /**
  * @public
  */
@@ -77,25 +82,16 @@ export const NETWORK_RETRY_INTERVAL = 2000;
 /**
  * @public
  */
-export const DEFAULT_SYNC_INTERVAL = 30000;
+export const NETWORK_TIMEOUT = 7000;
 /**
  * @public
  */
-export const MINIMUM_SYNC_INTERVAL = 3000;
+export const PUT_APP_INFO_MESSAGE = 'put appinfo';
 /**
  * @public
  */
-export const DEFAULT_CONFLICT_RESOLUTION_STRATEGY = 'ours-diff';
+export const SET_DATABASE_ID_MESSAGE = 'set database id';
 /**
  * @public
  */
-export const DEFAULT_COMBINE_DB_STRATEGY = 'combine-head-with-theirs';
-/**
- * @public
- */
-export const DUPLICATED_FILE_POSTFIX = '-from-';
-
-/**
- * @public
- */
-export const JSON_EXT = '.json';
+export const SHORT_SHA_LENGTH = 7;
