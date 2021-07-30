d533346d851212f36b8f5404ba11c7813fb8c659
sosuisen <hidekazu.kubota@gmail.com>
2021-07-31 01:32:04 +0900

    test: move offline tests from remote_base to remote_offline

--- test/remote_offline/json_diff.test.ts
+++ test/remote_offline/json_diff.test.ts
@@ -1,4 +1,11 @@
 /* eslint-disable @typescript-eslint/naming-convention */
+/**
+ * GitDocumentDB
+ * Copyright (c) Hidekazu Kubota
+ *
+ * This source code is licensed under the Mozilla Public License Version 2.0
+ * found in the LICENSE file in the root directory of this source tree.
+ */
 import expect from 'expect';
 import { JsonDiff } from '../../src/remote/json_diff';
 
